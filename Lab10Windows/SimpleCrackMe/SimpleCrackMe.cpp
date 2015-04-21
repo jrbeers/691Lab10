b87bc38ff4d9f493856b670e925a28e999ad2782
sterbap1 <sterbap1@gmail.com>
2015-04-20 21:06:33 -0400

    fix assembly

--- Lab10Windows/SimpleCrackMe/SimpleCrackMe.cpp
+++ Lab10Windows/SimpleCrackMe/SimpleCrackMe.cpp
@@ -11,7 +11,6 @@
 #define JUNK_CODE_ONE        \
     __asm{push eax}            \
     __asm{xor eax, eax}        \
-	__asm{ror ecx}			\
     __asm{setpo al}            \
     __asm{push edx}            \
     __asm{xor edx, eax}        \
@@ -19,7 +18,6 @@
     __asm{xchg eax, edx}    \
     __asm{pop edx}            \
     __asm{or eax, ecx}        \
-	__asm{rol ecx}				\
     __asm{pop eax}
 
 BOOL doCheck(char user[], unsigned char* key);
